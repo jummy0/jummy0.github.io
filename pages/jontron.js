ed7019a0acd6e6a75918447b7208c5f0e63c37ba
Jimmy Kunze <jimmyk3_14159@gmail.com>
2015-05-18 18:12:36 -0500

    Revert "+ index and styles"
    This reverts commit 148aeb80145df52dd2ea74ad54bd1807aeaa25db.
    
    Conflicts:
    	default.css

---
+++ pages/jontron.js
@@ -0,0 +1,10 @@
+var request = new XMLHttpRequest();
+request.onload = function() {
+var fileContent = this.responseText;
+var fileContentLines = fileContent.split( '\n' );
+var randomLineIndex = Math.floor( Math.random() * fileContentLines.length );
+var randomLine = fileContentLines[ randomLineIndex ];
+document.getElementById( 'random-phrase' ).innerHTML = randomLine;
+};
+request.open( 'GET', 'pages/jontron.txt', true );
+request.send();
