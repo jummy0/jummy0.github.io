3b7daaad844e7ae07089ca0a7597ebb56fb7c499
Jimmy Kunze <jimmyk3_14159@gmail.com>
2015-05-18 21:14:18 -0500

    more YT links on jontron.txt

--- pages/jontron.js
+++ pages/jontron.js
@@ -5,6 +5,7 @@ var fileContentLines = fileContent.split( '\n' );
 var randomLineIndex = Math.floor( Math.random() * fileContentLines.length );
 var randomLine = fileContentLines[ randomLineIndex ];
 document.getElementById( 'random-phrase' ).innerHTML = randomLine;
+document.getElementById( 'phrase-count' ).innerHTML = fileContentLines.length;
 };
 request.open( 'GET', '/pages/jontron.txt', true );
 request.send();
