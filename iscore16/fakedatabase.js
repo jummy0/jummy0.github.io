baea797f74b34c0502ac4d9634d1d09e616dfbde
Jimmy <jimmyk3.14159@gmail.com>
2016-05-03 10:27:28 -0500

    Create fakedatabase.js

---
+++ iscore16/fakedatabase.js
@@ -0,0 +1,20 @@
+var request = new XMLHttpRequest();
+var data = [];
+request.onload = function() {
+	var fileContent = this.responseText;
+	var fileContentRows = fileContent.split( '\n' ).trim();
+	for (i = 0; i < fileContentRows.length; i++) {
+		data[i] = fileContentRows.split('/').trim();
+	}
+};
+request.open( 'GET', 'fake.database', true );
+request.send();
+var placementTableContent = '';
+for (i = 0; i < data.length; i++) {
+	placementTableContent += '<tr>';
+	for (j = 0; j < data[i].length; j++) {
+		placementTableContent += '<td>' + data[i][j] + '</td>';
+	}
+	placementTableContent += '</tr>';
+}
+document.getElementById('placement-table').innerHTML = placementTableContent;
