2c603288d12b77eab9493c5fe7342a008f35e977
Jimmy <jimmyk3.14159@gmail.com>
2016-05-10 10:14:43 -0500

    Update database.js

--- iscore16/database.js
+++ iscore16/database.js
@@ -1,29 +1,15 @@
 var request = new XMLHttpRequest();
-var data = [];
 var fileContent = '';
-request.onload = function() {
-	fileContent = this.responseText;
-	var fileContentRows = fileContent.split('\n').map(Function.prototype.call, String.prototype.trim);
-	for (i = 0; i < fileContentRows.length; i++) {
-		data[i] = fileContentRows[i].split('/').map(Function.prototype.call, String.prototype.trim);
-	}
-	var placementTableContent = '';
-	for (i = 0; i < data.length; i++) {
-		placementTableContent += '<tr>';
-		for (j = 0; j < data[i].length; j++) {
-			if (j == 1) {
-				placementTableContent += '<td><img src="logo/' + data[i][j] + '"></td>';
-			} else if (j == 2) {
-				placementTableContent += '<td>Team&nbsp;' + data[i][j] + '</td>';
-			} else if (j == 3 && i == 0) {
-				placementTableContent += '<td><div class="scrolling-table-cell">' + data[i][j] + '</div></td>';
-			} else {
-				placementTableContent += '<td>' + data[i][j] + '</td>';
-			}
+function readDatabase(location) {
+	var data = [];
+	request.onload = function() {
+		fileContent = this.responseText;
+		var fileContentRows = fileContent.split('\n').map(Function.prototype.call, String.prototype.trim);
+		for (i = 0; i < fileContentRows.length; i++) {
+			data[i] = fileContentRows[i].split('/').map(Function.prototype.call, String.prototype.trim);
 		}
-		placementTableContent += '</tr>';
-	}
-	document.getElementById('placement-data').innerHTML = placementTableContent;
-};
-request.open( 'GET', 'database.dat', true );
-request.send();
+	};
+	request.open( 'GET', location, true );
+	request.send();
+	return data;
+}
